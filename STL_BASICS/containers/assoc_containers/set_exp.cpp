55249b09dc4a18b3ca14707b992db77aaa6d88a5
v33rh0ra <tanveer.hora@gmail.com>
2018-07-28 04:10:28 +0530

    STL_BASICS - revised

---
+++ STL_BASICS/containers/assoc_containers/set_exp.cpp
@@ -0,0 +1,43 @@
+/*
+* set
+* - No Duplicates
+*/
+#include <set>
+#include <iostream>
+using namespace std;
+
+void display (set<int>& myset){
+    for(set<int>::iterator it=myset.begin();it!=myset.end();++it){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    set<int> myset;
+    myset.insert(3); // myset: {3}
+    myset.insert(1); // myset: {1,3}
+    myset.insert(7); // myset: {1,3,7}, O(log(n))
+
+    display(myset);
+    
+    set<int>::iterator it;
+
+    it = myset.find(7); //O(log(n)), it points to 7
+    //sequence containers do not have find() member function
+    pair<set<int>::iterator,bool> ret;
+    ret = myset.insert(3); //no new element inserted
+    if(ret.second==false){
+        it = ret.first; //"it" points to element 3
+    }
+    //The position where 9 will be inserted cannot be decided by you
+    //Since this is an ordered set, the poistion will be decided
+    //based on its own value
+    myset.insert(it,9); // myset: {1, 3, 7, 9} O(log(n)) => O(1)
+    display(myset);                    // it points to 3
+    myset.erase(it); //myset:{1,7,9}
+    display(myset);
+    myset.erase(7); //myset:{1,9}
+    display(myset);
+    return 0;
+
+}
